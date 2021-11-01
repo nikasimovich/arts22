9f942b49a18ff39cfd3cf6d951155b1ca8b3b670
nikaclass <nika.f@columbia.edu>
2021-11-01 12:40:24 -0400

    Add js

--- assets/demos/responsive-layout-review-js/assets/main.js
+++ assets/demos/responsive-layout-review-js/assets/main.js
@@ -1,1 +1,14 @@
-console.log ("hello");
+let body = document.body;
+let mobilenav = document.querySelector(".mobile-nav");
+let closeMenu = document.querySelector(".close");
+
+function addMobileNav() {
+  mobilenav.classList.toggle("hide");
+  mobilenav.classList.toggle("show");
+}
+
+let button = document.querySelector(".mobile-nav-button");
+button.addEventListener("click", addMobileNav);
+
+
+closeMenu.addEventListener("click", addMobileNav);
